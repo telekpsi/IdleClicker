79a17e7a0ce5744856e0d01b8c49e9a39e7abd41
David Evander <davidevander@gmail.com>
2017-03-01 00:32:13 -0500

    coded most of RollMonster and setup portraits

--- src/IdleClicker.java
+++ src/IdleClicker.java
@@ -1,45 +1,54 @@
 /*
  David Evander
  
--start screen that comes up when game runs asking hero's name
 -implement items that boost stats
--monster stats are somewhat random within range of hero
--set up attack action
--update health method for hero and monster
+-monster stats should be somewhat random within range of hero
+-set up attack button actions
+--update health method for hero and monster
 -timer based attack by monster
--possible speed implementation for monster
+--possible speed implementation for monster
 -if hero's health hits 0, death screen, offer to start over
 -when monster dies, hero gets exp(check for level up), 1/10 chance of item drop
--hero and monster images
--monster name and picture relation (randomized) 
-
+-maybe a separate method that updates the scattered instances of name/strength/etc.
+-make start question Fancy
 */
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
 
 public class IdleClicker extends JFrame{
 	
 	private Hero hero;
+	private Boolean isHeroDead = false;
 	private Monster monster;
 	
 	public static void main(String[] args) {
 		IdleClicker game = new IdleClicker();
 		game.setSize(600,350);
+		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		game.setVisible(true);
-		
 	}	
 	
 	private JLabel lblLvlCnt, lblAttCnt, lblDefCnt, lblHltCnt, lblExpCnt, lblName;
 	private JLabel lblMLvlCnt, lblMAttCnt, lblMDefCnt, lblMHltCnt, lblMExpCnt, lblMName;
+	private JLabel picLabelMonster;
+	private BufferedImage hisPicture = null;
 	
 	public IdleClicker() {
+		//use constructors and sync to privates
 		Hero you = new Hero();
 		hero=you;
 		Monster him = new Monster();
 		monster=him;
-		System.out.println(hero.att);
+		
+				
+		//gui
+		setTitle("Click for Greatness");
 		JPanel panel = new JPanel();
 		getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
@@ -100,14 +109,6 @@ public class IdleClicker extends JFrame{
 		lblExpCnt.setBounds(537, 220, 46, 14);
 		panel.add(lblExpCnt);
 		
-		JButton btnAttack = new JButton("Attack!");
-		btnAttack.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnAttack.setBounds(247, 263, 89, 23);
-		panel.add(btnAttack);
-		
 		JLabel lblMExp = new JLabel("EXP:");
 		lblMExp.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblMExp.setBounds(10, 220, 50, 14);
@@ -157,6 +158,54 @@ public class IdleClicker extends JFrame{
 		lblMName.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMName.setBounds(10, 30, 90, 14);
 		panel.add(lblMName);
+		
+		
+		//show off the handsome hero
+		BufferedImage myPicture = null;
+		try {
+			myPicture = ImageIO.read(new File("hero.jpg"));
+		} catch (IOException e) {
+			System.out.println("MY BEAUTIFUL PORTRAIT IS MISSING!");
+		}
+		JLabel picLabelHero = new JLabel(new ImageIcon(myPicture));
+		picLabelHero.setBounds(310, 40, 160, 200);
+		panel.add(picLabelHero);
+		
+		//get hero's name
+		Component p=panel;
+		Object message="What is your name?";
+		Object name2=JOptionPane.showInputDialog(p, message, "who dis?", JOptionPane.QUESTION_MESSAGE,null ,null,"Hero");
+		if (name2==null){
+			
+		}else{
+			hero.name=name2.toString();
+			you.name=hero.name;
+			lblName.setText(hero.name);
+		}
+		
+		//initial monster will always be a goblin
+		try {
+			hisPicture = ImageIO.read(new File("goblin.jpg"));
+		} catch (IOException e) {
+			System.out.println("The goblin must be underfoot somewhere");;
+		}
+		picLabelMonster = new JLabel(new ImageIcon(hisPicture));
+		picLabelMonster.setBounds(140, 40, 160, 200);
+		panel.add(picLabelMonster);
+		
+		
+		//attack button uses hero's attack and monster's defense to calculate loss of monster's health
+		JButton btnAttack = new JButton("Attack!");
+		btnAttack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnAttack.setBounds(247, 263, 89, 23);
+		panel.add(btnAttack);
+		
+		
+		//gonna need some loops to keep everything going
+		//set the jlabels for names and stats and delete/add of portraits after every call of RollMonster
 	}
 
 	//maybe should make private
@@ -234,12 +283,12 @@ public class IdleClicker extends JFrame{
 		
 			//constructor
 		public Monster(){
-				att = 0;
+				att = 1;
 				def = 0;
-				lvl = 0;
-				hltNow=0;
-				hltMax=0;
-				expVal=0;
+				lvl = 1;
+				hltNow=3;
+				hltMax=3;
+				expVal=1;
 				name="Monster";
 				//maybe a speed stat?
 		}
@@ -289,8 +338,126 @@ public class IdleClicker extends JFrame{
 		}
 	}
 	
-	public void MakeMon(int level){
+	//randomized stats based on hero's level (or maybe his stats)
+	//switch statement off randomizer to pick monster name and portrait
+	//later on could individualize stat ranges for individual monsters based on initial roll
+	public void RollMonster() {
+		//stats go here
 		
+		//rand and switch
+		int randMonster=(((int)(Math.random()*100)%10)+1);
+		switch (randMonster) {
+			case 1:
+				try {
+					hisPicture = ImageIO.read(new File("goblin.jpg"));
+				} catch (IOException e) {
+					System.out.println("The goblin must be underfoot somewhere");
+				}
+				picLabelMonster = new JLabel(new ImageIcon(hisPicture));
+				picLabelMonster.setBounds(140, 40, 160, 200);
+				monster.name="Goblin";
+				return;
+			case 2:
+				try {
+					hisPicture = ImageIO.read(new File("Bodak.jpg"));
+				} catch (IOException e) {
+					System.out.println("I want to believe");
+				}
+				picLabelMonster = new JLabel(new ImageIcon(hisPicture));
+				picLabelMonster.setBounds(140, 40, 160, 200);
+				monster.name="Bodak";
+				return;
+			case 3:
+				try {
+					hisPicture = ImageIO.read(new File("kaiju.jpg"));
+				} catch (IOException e) {
+					System.out.println("FISHSTICKS");
+				}
+				picLabelMonster = new JLabel(new ImageIcon(hisPicture));
+				picLabelMonster.setBounds(140, 40, 160, 200);
+				monster.name="Kaiju";
+				return;
+			case 4:
+				try {
+					hisPicture = ImageIO.read(new File("skeleton.jpg"));
+				} catch (IOException e) {
+					System.out.println("Have we checked in the closet yet?");
+				}
+				picLabelMonster = new JLabel(new ImageIcon(hisPicture));
+				picLabelMonster.setBounds(140, 40, 160, 200);
+				monster.name="Skeleton";
+				return;
+			case 5:
+				try {
+					hisPicture = ImageIO.read(new File("hunch.jpg"));
+				} catch (IOException e) {
+					System.out.println("He can't have shuffled far!");
+				}
+				picLabelMonster = new JLabel(new ImageIcon(hisPicture));
+				picLabelMonster.setBounds(140, 40, 160, 200);
+				monster.name="Hunchback";
+				return;
+			case 6:
+				try {
+					hisPicture = ImageIO.read(new File("gir.jpg"));
+				} catch (IOException e) {
+					System.out.println("Doom doom doom doom doom doom doom");
+				}
+				picLabelMonster = new JLabel(new ImageIcon(hisPicture));
+				picLabelMonster.setBounds(140, 40, 160, 200);
+				monster.name="Gir";
+				return;
+			case 7:
+				try {
+					hisPicture = ImageIO.read(new File("creeper.jpg"));
+				} catch (IOException e) {
+					System.out.println("C'mon gang, let's split up and look for clues!");
+				}
+				picLabelMonster = new JLabel(new ImageIcon(hisPicture));
+				picLabelMonster.setBounds(140, 40, 160, 200);
+				monster.name="Creeper";
+				return;
+			case 8:
+				try {
+					hisPicture = ImageIO.read(new File("bear.jpg"));
+				} catch (IOException e) {
+					System.out.println("LeoDC was here");
+				}
+				picLabelMonster = new JLabel(new ImageIcon(hisPicture));
+				picLabelMonster.setBounds(140, 40, 160, 200);
+				monster.name="Bear";
+				return;
+			case 9:
+				try {
+					hisPicture = ImageIO.read(new File("vampire.jpg"));
+				} catch (IOException e) {
+					System.out.println("He doesn't show up well in photos anyway");
+				}
+				picLabelMonster = new JLabel(new ImageIcon(hisPicture));
+				picLabelMonster.setBounds(140, 40, 160, 200);
+				monster.name="Vampire";
+				return;
+			case 10:
+				try {
+					hisPicture = ImageIO.read(new File("dragon.jpg"));
+				} catch (IOException e) {
+					System.out.println("WHERE ARE MY DRAGONS");
+				}
+				picLabelMonster = new JLabel(new ImageIcon(hisPicture));
+				picLabelMonster.setBounds(140, 40, 160, 200);
+				monster.name="Dragon";
+				return;
+			default:
+				try {
+					hisPicture = ImageIO.read(new File("goblin.jpg"));
+				} catch (IOException e) {
+					System.out.println("The goblin must be underfoot somewhere");
+				}
+				picLabelMonster = new JLabel(new ImageIcon(hisPicture));
+				picLabelMonster.setBounds(140, 40, 160, 200);
+				monster.name="Goblin";
+				return;
+		}
 	}
 	
 	//once expNow==expNext, hero's stats get a boost
